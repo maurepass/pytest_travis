0818eb58c4cb74b0891c0f32a7f38ad78488f999
maurepass <maurepass@gmail.com>
2020-03-23 22:29:50 +0100

    add python files

---
+++ twitter.py
@@ -0,0 +1,7 @@
+class Twitter:
+
+    def __init__(self):
+        self.tweets = []
+
+    def tweet(self, message):
+        self.tweets.append(message)
---
+++ twitter_test.py
@@ -0,0 +1,12 @@
+from twitter import Twitter
+
+
+def test_twitter_initialization():
+    twitter = Twitter()
+    assert twitter
+
+
+def test_tweet_singe_message():
+    twitter = Twitter()
+    twitter.tweet('Test message')
+    assert twitter.tweets == ['Test message']
