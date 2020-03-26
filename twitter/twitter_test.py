7dd6f5f570a843e25e36c25730444b601cec15f9
maurepass <maurepass@gmail.com>
2020-03-26 18:55:20 +0100

    add new branch

--- twitter/twitter_test.py
+++ twitter/twitter_test.py
@@ -16,3 +16,9 @@ def test_tweet_next_message():
     twitter = Twitter()
     twitter.tweet('New message')
     assert twitter.tweets == ["New message"]
+
+
+def test_pull_new_branch():
+    twitter = Twitter()
+    twitter.tweet("New branch")
+    assert twitter.tweets == ["New branch"]
